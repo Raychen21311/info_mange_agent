b02ccb4db2c51bf2a0cf71da96b5af5652ae9cad
Raychen21311 <32985938+Raychen21311@users.noreply.github.com>
2025-09-23 13:24:45 +0800

    Update try_dm.py

--- try_dm.py
+++ try_dm.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 print("目前工作目錄：", os.getcwd())
 print("try_dm.py 所在目錄：", Path(__file__).resolve().parent)
 print("檔案是否存在？", (Path(__file__).resolve().parent / "file_path" / "faiss_index.enc").exists())
